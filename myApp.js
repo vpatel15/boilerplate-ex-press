e7ff19a088963fceabe0e245fbfb96c30ef4d411
Vaibhav Patel <vaibhav150189@yahoo.com>
2022-05-06 07:17:04 +0000

    set response according to global env variables

--- myApp.js
+++ myApp.js
@@ -15,6 +15,15 @@ app.get('/', function(req, res) {
 // Serve static assests like css js etc
 app.use('/public', express.static(__dirname + '/public'))
 
+// Serve Json from REST API request
+app.use('/json', function(req, res) {
+  if(process.env.MESSAGE_STYLE === 'uppercase') {
+    res.json({"message": "HELLO JSON"}); 
+  } else {
+    res.json({"message": "Hello json"});
+  }
+})
+
 
 
 
