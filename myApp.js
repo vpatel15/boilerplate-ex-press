0909ca1ba93bc13ae6ff411eebe80a7ba50823ec
Vaibhav Patel <vaibhav150189@yahoo.com>
2022-05-06 06:46:24 +0000

    send file as response

--- myApp.js
+++ myApp.js
@@ -3,8 +3,12 @@ let app = express();
 
 console.log('Hello World');
 
+// app.get('/', function(req, res) {
+//   res.send('Hello Express');
+// })
+
 app.get('/', function(req, res) {
-  res.send('Hello Express');
+  res.sendFile(__dirname + '/views/index.html');
 })
 
 
